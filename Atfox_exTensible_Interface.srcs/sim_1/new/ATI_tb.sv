ab5db43de8b7b2916f031b1dbdb78972b592765f
atfox272 <trananhtai577@gmail.com>
2023-11-11 21:47:17 +0700

    Initial commit

---
+++ Atfox_exTensible_Interface.srcs/sim_1/new/ATI_tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/11/2023 09:41:18 PM
+// Design Name: 
+// Module Name: ATI_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ATI_tb(
+
+    );
+endmodule
